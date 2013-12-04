03938912758cfef8c4909ab779d7d4a39a38fc29
Mark D Holmberg <mark.d.holmberg@gmail.com>
2013-12-03 19:12:55 -0700

    Adds support for title helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def title(page_title, show_sub_title = true, options={})
+    content_for(:title) { page_title }
+    if show_sub_title
+      sub_title(page_title)
+    end
+  end
+
+  def sub_title(sub_title, options={})
+    content_for(:sub_title) { sub_title }
+  end
 end
