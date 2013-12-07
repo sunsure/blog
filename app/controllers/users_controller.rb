a4b4d5bba5f722a23150086ade0ffde4add01b54
Mark D Holmberg <mark.d.holmberg@gmail.com>
2013-12-06 19:19:20 -0700

    Starts adding support for user controllers and roles. #7.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,62 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(safe_params)
+
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if safe_params[:password].blank?
+      safe_params.delete("password")
+      safe_params.delete("password_confirmation")
+
+      if @user.update_without_password(safe_params)
+        redirect_to @user, notice: 'User was successfully updated.'
+      else
+        render :edit
+      end
+    else
+      if @user.update(safe_params)
+        redirect_to @user, notice: 'User was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_url, notice: 'User was successfully removed.'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def safe_params
+    params.require(:user).permit(:email, :password, :password_confirmation, role_ids: [])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Blog::Application.routes.draw do
 
   devise_for :users, path: "u", path_names: { sign_in: 'login', sign_out: 'logout', confirmation: 'confirm', registration: 'register', sign_up: 'new' }
+
   match :dashboard, to: "dashboard#index", via: [:get]
   resources :articles
+  resources :users
+
   root 'articles#index'
 
 end
